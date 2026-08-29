0fba44d823f94bac7233008f2bb867d7e865779b
Giorgi <giobuisness97@gmail.com>
2026-08-29 23:26:48 +0400

    Initial Commit

---
+++ RealMac/RealMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealMacApp.swift
+//  RealMac
+//
+//  Created by giorgi giorgadze on 29/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct RealMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
